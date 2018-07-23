1e3a8c072b940aa60d31215bcc8074cb9ab12726
Craig Bowditch <craigbowditch@hotmail.com>
2018-07-23 18:14:05 +0100

    creates class for team

---
+++ specs/team_spec.rb
@@ -0,0 +1,18 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative ('../team.rb')
+
+class TeamTest < MiniTest::Test
+
+
+  def setup
+    @team = Team.new("Team Good Guys", ["Craig", "John", "Paul"], "Coach Big Man")
+    
+
+
+
+
+
+
+
+end
---
+++ team.rb
@@ -0,0 +1,9 @@
+class Team
+
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+  end
+
+end
